7683a5fa0e47fcff2c2aeca1963eb37db318b4be
Naresh Surisetty <nareshskyrocket@gmail.com>
2018-07-04 17:19:37 +0530

    Minor Changes to hoime App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,14 +6,14 @@ import { GET_ALL_JOBS } from '../queries';
 
 const App = () =>(
   <div className="App">
-  <h1>Home</h1>
+  <h1>Welcome to Verizon Job Portal!!</h1>
   <Query query={GET_ALL_JOBS}>
     {({data, loading, error}) =>{
       if (loading) return <div>Loading</div>
       if (error) return <div>Error</div>
       console.log(data);
       return (
-        <p>Jobs</p>
+        <p>Currently No Open Positions..</p>
       )
     } }
   </Query>
